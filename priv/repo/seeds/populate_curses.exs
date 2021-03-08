c230423dd2d9588f37ead951f779be068b7c18be
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-03-07 22:09:19 -0300

    feat: add seeds for curses

--- priv/repo/seeds/populate_curses.exs
+++ priv/repo/seeds/populate_curses.exs
@@ -3,8 +3,24 @@ defmodule AnnoyingBot.Repo.Seeds.PopulateCurses do
 
   Repo.delete_all(Curse)
 
-  AnnoyingBot.create_curse(%{phrase: "{user} saco de bosta"})
-  AnnoyingBot.create_curse(%{phrase: "{user} saco de lixo"})
-  AnnoyingBot.create_curse(%{phrase: "{user} horrível"})
-  AnnoyingBot.create_curse(%{phrase: "{user} ruim"})
+  @curse_long_phrase "Se o {user} acha, eu discordo.\nSe o {user} fala, eu fico surdo.\nSe o {user} erra, eu comemoro.\nSe o {user} tem 100 haters, eu sou um deles.\nSe o {user} tem 10 haters, eu sou um deles.\nSe o {user} tem 1 hater, eu sou esse hater.\nSe o {user} não tem haters, eu não existo."
+
+  # GENERAL TYPE
+  AnnoyingBot.create_curse(%{phrase: "{user} saco de bosta", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: "{user} saco de lixo", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: "{user} horrível", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: "{user} ruim", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: "{user} sua mãe nasceu pelada", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: "{user} arrombado", type: "general"})
+  AnnoyingBot.create_curse(%{phrase: @curse_long_phrase, type: "general"})
+
+  # ONLINE TYPE
+  AnnoyingBot.create_curse(%{phrase: "O corno {user} acordou", type: "online"})
+  AnnoyingBot.create_curse(%{phrase: "eae {user} seu corno, tá on pq?", type: "online"})
+  AnnoyingBot.create_curse(%{phrase: "eae {user} bom dia arrombado", type: "online"})
+
+  # OFFLINE TYPE
+  AnnoyingBot.create_curse(%{phrase: "O corno {user} foi dormir", type: "offline"})
+  AnnoyingBot.create_curse(%{phrase: "vai dormir {user} aproveita e morre", type: "offline"})
+  AnnoyingBot.create_curse(%{phrase: "{user} kitou e foi dormir", type: "offline"})
 end
