7e63f69afa0cf9d00110ad1f6464c10c390110af
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-03-10 22:54:22 -0300

    build: set correct cowboy version

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule AnnoyingBot.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:cowboy, "< 2.8.0", override: true}
     ]
   end
 
