8237a0333c33a1f0686e02574c7ffd57414152f6
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-03-17 12:28:37 -0300

    refactor(discord): decreases probability of reply message

--- lib/annoying_bot/discord/handlers/reply_message_handler.ex
+++ lib/annoying_bot/discord/handlers/reply_message_handler.ex
@@ -2,16 +2,16 @@ defmodule AnnoyingBot.Discord.ReplyMessageHandler do
   alias AnnoyingBot.Discord.ReplyMessageCommand
 
   def call(msg) do
-    with :ok <- curse_by_lucky() do
+    with :unlucky <- curse_by_lucky() do
       ReplyMessageCommand.call(msg)
     end
   end
 
   defp curse_by_lucky() do
-    random_index = Enum.random(0..12)
+    random_index = Enum.random(1..12)
 
-    with true <- random_index < 6 do
-      :ok
+    with true <- random_index <= 2 do
+      :unlucky
       else _ -> :lucky
     end
   end
